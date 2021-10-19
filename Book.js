567ab3674a368c2ada9fe2de1dbe3053d40b9a76
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2021-10-19 19:27:38 +0300

    Fix Linter errors

--- Book.js
+++ Book.js
@@ -1,53 +1,45 @@
-class Book { // eslint-disable-line
+class Book { 
   constructor(title, author, key) {
     this.title = title;
     this.author = author;
     this.key = key;
   }
-}
 
-class Store {
-  static getBooksFromStorage() {
+static  getBooksFromStorage() {
     let books;
     if (localStorage.getItem('books') === null) {
       books = [];
     } else {
       books = JSON.parse(localStorage.getItem('books'));
     }
-
-    return books;
+        return books;
   }
 
-  static addBookToStorage(book) {
-    const books = Store.getBooksFromStorage();
+ static  addBookToStorage(book) {
+    const books = Book.getBooksFromStorage();
     books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
   }
 
-  static removeBook(key) {
-    const books = Store.getBooksFromStorage();
+ static  removeBook(key) {
+    const books = Book.getBooksFromStorage();
     books.forEach((book, i) => {
-      if (book.key == key) { // eslint-disable-line
+     let key1 = parseInt(key);
+      if (key1 === book.key ) { 
         books.splice(i, 1);
       }
-    });
-
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-}
-
-class Display {
-  static displayBooks() {
-    const books = Store.getBooksFromStorage();
-
-    books.forEach((book) => Display.addBookToDisplay(book));
+     });
+      localStorage.setItem('books', JSON.stringify(books));
   }
 
-  static addBookToDisplay(book) {
+ static displayBooks() {
+    const books = Book.getBooksFromStorage();
+     books.forEach((book) => Book.addBookToDisplay(book));
+  }
+
+ static addBookToDisplay(book) {
     const list = document.querySelector('.book-list');
-
     const row = document.createElement('tr');
-
     row.innerHTML = `
       <h2>${book.title}</h2>
       <h2>${book.author}</h2>
@@ -58,27 +50,33 @@ class Display {
     list.appendChild(row);
   }
 
-  static deleteBookFromDisplay(el) {
+ static deleteBookFromDisplay(el) {
     if (el.classList.contains('delete')) {
       el.parentElement.parentElement.remove();
     }
   }
 }
 
-document.addEventListener('DOMContentLoaded', Display.displayBooks);
+document.addEventListener('DOMContentLoaded', Book.displayBooks);
 
-document.querySelector('#book-form').addEventListener('submit', () => {
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const key = Math.floor(Math.random() * 100000000);
-
+  
+  if(title === '' || author === '') {
+    window.alert('Please fill in Both fields');
+  } else {
   const book = new Book(title, author, key);
 
-  Display.addBookToDisplay(book);
-  Store.addBookToStorage(book);
-});
+  Book.addBookToDisplay(book);
+  Book.addBookToStorage(book);
+
+}});
 
 document.querySelector('.book-list').addEventListener('click', (e) => {
-  Display.deleteBookFromDisplay(e.target);
-  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+  Book.deleteBookFromDisplay(e.target);
+  Book.removeBook(e.target.parentElement.previousElementSibling.textContent);
 });

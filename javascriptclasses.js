f64c0d86682c0a7435296ba6c02bd098f40646c6
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2021-10-19 17:26:41 +0300

    Add Javasctipt classes

---
+++ javascriptclasses.js
@@ -0,0 +1,86 @@
+class Book {
+  constructor(title, author, key) {
+    this.title = title;
+    this.author = author;
+    this.key = key;
+  }
+}
+
+class Display {
+  static displayBooks() {
+    const books = Store.getBooksFromStorage();
+
+    books.forEach((book) => Display.addBookToDisplay(book));
+  }
+
+  static addBookToDisplay(book) {
+    const list = document.querySelector(".book-list");
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <h2>${book.title}</h2>
+      <h2>${book.author}</h2>
+      <h2 hidden>${book.key}</h2>
+      <h2><button class="btn btn-sm delete" id="button">Remove</button></h2>
+    `;
+
+    list.appendChild(row);
+  }
+
+  static deleteBookFromDisplay(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+}
+
+class Store {
+  static getBooksFromStorage() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+
+    return books;
+  }
+
+  static addBookToStorage(book) {
+    const books = Store.getBooksFromStorage();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(key) {
+    const books = Store.getBooksFromStorage();
+    books.forEach((book, i) => {
+      console.log(book);
+      
+      if (book.key == key) {
+        books.splice(i, 1);
+      }
+    });
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
+document.addEventListener("DOMContentLoaded", Display.displayBooks);
+
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const key = Math.floor(Math.random() * 100000000);
+
+  
+  const book = new Book(title, author, key);
+
+  Display.addBookToDisplay(book);
+  Store.addBookToStorage(book);
+});
+
+document.querySelector(".book-list").addEventListener("click", (e) => {
+  Display.deleteBookFromDisplay(e.target);
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+});
